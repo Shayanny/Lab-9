6d2c28d90cae988e0d30e3acb9472b6f7ce5c5ad
Shayanny <g00400975@atu.ie>
2024-04-26 11:48:38 +0000

    Finished lab 9

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Geolocation } from '@capacitor/geolocation';
+import { Browser } from '@capacitor/browser';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, 
+    IonContent , IonButton, IonButtons],
 })
+
 export class HomePage {
+
+   coordinates:any= "";
+   lat:number = 0;
+   long:number = 0;
+   time:any = 0;
+
   constructor() {}
+
+  async getGPS(){
+
+    this.coordinates = await Geolocation.getCurrentPosition();
+    this.long = this.coordinates.coords.longitude;
+    this.lat = this.coordinates.coords.latitude;
+    this.time = new Date(this.coordinates.timestamp);
+
+  }
+
+    openCapacitorSite= async () =>{
+
+    await Browser.open({ url: 'http://capacitorjs.com/' });
+  
+    };
+
+
 }
+
+
